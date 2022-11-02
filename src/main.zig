2aace56ede04cdf7e5a9cf9ce618c051e72f7a57
apple-x-co <apple.x.co@gmail.com>
2022-11-02 09:02:31 +0900

    commit

--- src/main.zig
+++ src/main.zig
@@ -20,18 +20,22 @@ pub fn main() !void {
 
     const seconds = try std.fmt.parseInt(u64, paramSeconds, 10);
 
+    // TODO: ローディングアニメーション
+    // ⠿
+    // ⠷
+    // ⠯
+    // ⠟
+    // ⠻
+    // ⠽
+    // ⠾
+
     var i: u64 = 0;
     while (i <= seconds) : (i += 1) {
         try writer.print("[\x1b[2K[\u{001b}[42m\u{001b}[1m", .{});
 
-        for (progressBar(i, seconds)) |bar| {
-            std.debug.print("{c}", .{bar});
-            // try writer.print("{c}", .{bar}); // FIXME: 文字化ける
-        }
-
-        // FIXME: 以下もNG
-        // var s = progressBar(i, seconds);
-        // try writer.print("{s}", .{s});
+        var strings = try progressBar(allocator, i, seconds);
+        defer strings.deinit();
+        try writer.print("{s}", .{strings.items});
 
         try writer.print("\u{001b}[0m] \u{001b}[32m{}/{}\u{001b}[0m\r", .{seconds - i, seconds});
 
@@ -41,27 +45,27 @@ pub fn main() !void {
     try writer.print("\n", .{});
 }
 
-fn progressBar(currentTime: u64, totalTime: u64) []const u8 {
+fn progressBar(allocator: std.mem.Allocator, currentTime: u64, totalTime: u64) anyerror!std.ArrayList(u8) {
     const maxWidth = 50;
     const ratio = @intToFloat(f64, currentTime) / @intToFloat(f64, totalTime);
     const current = @floatToInt(u64, maxWidth * ratio);
 
-    var strings = [_]u8{' '} ** maxWidth;
+    var strings = std.ArrayList(u8).init(allocator);
 
     var i: u64 = 0;
     while (i < maxWidth) : (i += 1)  {
         if (i < current) {
-            strings[i] = '=';
+            try strings.append('=');
             continue;
         }
 
         if (i == current) {
-            strings[i] = '>';
+            try strings.append('>');
             continue;
         }
 
-        break;
+        try strings.append(' ');
     }
 
-    return strings[0..maxWidth];
+    return strings;
 }
