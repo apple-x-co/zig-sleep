10fc1c323e2368de8e137d450d9e9078e788ab57
apple-x-co <apple.x.co@gmail.com>
2022-11-03 17:47:13 +0900

    commit

--- src/main.zig
+++ src/main.zig
@@ -18,26 +18,38 @@ pub fn main() !void {
     const stdout = std.io.getStdOut();
     const writer = stdout.writer();
 
+    // TODO: 1秒以下の秒数
     const seconds = try std.fmt.parseInt(u64, paramSeconds, 10);
-
-    // TODO: ローディングアニメーション
-    // ⠿
-    // ⠷
-    // ⠯
-    // ⠟
-    // ⠻
-    // ⠽
-    // ⠾
 
     var i: u64 = 0;
     while (i <= seconds) : (i += 1) {
-        try writer.print("[\x1b[2K[\u{001b}[42m\u{001b}[1m", .{});
-
         var strings = try progressBar(allocator, i, seconds);
         defer strings.deinit();
-        try writer.print("{s}", .{strings.items});
 
-        try writer.print("\u{001b}[0m] \u{001b}[32m{}/{}\u{001b}[0m\r", .{seconds - i, seconds});
+        // TODO: リファクタリング
+        var loading = "⠿";
+        if (i % 7 == 1) {
+            loading = "⠷";
+        }
+        if (i % 7 == 2) {
+            loading = "⠯";
+        }
+        if (i % 7 == 3) {
+            loading = "⠟";
+        }
+        if (i % 7 == 4) {
+            loading = "⠻";
+        }
+        if (i % 7 == 5) {
+            loading = "⠽";
+        }
+        if (i % 7 == 6) {
+            loading = "⠾";
+        }
+
+        try writer.print("[\x1b[2K(\u{001b}[46m\u{001b}[37m", .{});
+        try writer.print("{s}", .{strings.items});
+        try writer.print("\u{001b}[0m) {s} \u{001b}[1m\u{001b}[36m{}/{}\u{001b}[0m\r", .{loading, seconds - i, seconds});
 
         std.time.sleep(1000000000);
     }
@@ -54,8 +66,13 @@ fn progressBar(allocator: std.mem.Allocator, currentTime: u64, totalTime: u64) a
 
     var i: u64 = 0;
     while (i < maxWidth) : (i += 1)  {
+        if (maxWidth == current) {
+            try strings.append('*');
+            continue;
+        }
+
         if (i < current) {
-            try strings.append('=');
+            try strings.append('-');
             continue;
         }
 
@@ -64,7 +81,7 @@ fn progressBar(allocator: std.mem.Allocator, currentTime: u64, totalTime: u64) a
             continue;
         }
 
-        try strings.append(' ');
+        try strings.append('_');
     }
 
     return strings;
