3d73ab1f211e4f3359aabc915e700fc55cdd5eac
apple-x-co <apple.x.co@gmail.com>
2022-11-19 12:08:48 +0900

    commit

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const progressBarWidth = 20;
+const emoji1:[]const u8 = "🌲";
+const emoji2:[]const u8 = "🔅";
+const emoji3:[]const u8 = "🌱";
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -44,32 +49,27 @@ pub fn main() !void {
 
         currentSeconds = @intToFloat(f32, (milliseconds - i)) / @intToFloat(f32, std.time.ns_per_us);
 
-        try writer.print("[\x1b[2K(", .{});
+        try writer.print("[\x1b[2K🦉 ", .{});
         for (strings.items) |item| {
             try writer.print("{s}", .{ item });
         }
-        try writer.print(") {s} \u{001b}[37m\u{001b}[1m\u{001b}[36m {d:.1}/{d:.1} \u{001b}[0m\r", .{ loading, currentSeconds, totalSeconds });
+        try writer.print(" 🕊  {s} \u{001b}[37m\u{001b}[1m\u{001b}[36m {d:.1}/{d:.1} \u{001b}[0m\r", .{ loading, currentSeconds, totalSeconds });
 
         std.time.sleep(1 * std.time.ns_per_ms);
         i = timer.read() / std.time.ns_per_ms;
     }
 
-    try writer.print("[\x1b[2K\u{001b}[46m\u{001b}[37m FINISH!! {d:.1} seconds. \u{001b}[0m\n", .{totalSeconds});
+    try writer.print("[\x1b[2K\u{001b}[46m\u{001b}[37m FINISH 🙌 {d:.1} seconds. \u{001b}[0m\n", .{totalSeconds});
 }
 
 fn progressBar(allocator: std.mem.Allocator, currentTime: u64, totalTime: u64) anyerror!std.ArrayList([] const u8) {
-    const maxWidth = 10;
     const ratio = @intToFloat(f64, currentTime) / @intToFloat(f64, totalTime);
-    const current = @floatToInt(u64, maxWidth * ratio);
-
-    const emoji1:[]const u8 = "🌲";
-    const emoji2:[]const u8 = "🔅";
-    const emoji3:[]const u8 = "🌱";
+    const current = @floatToInt(u64, progressBarWidth * ratio);
 
     var strings = std.ArrayList([] const u8).init(allocator);
 
     var i: u64 = 0;
-    while (i < maxWidth) : (i += 1) {
+    while (i < progressBarWidth) : (i += 1) {
         if (i < current) {
             try strings.append(emoji1);
             continue;
